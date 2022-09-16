01bd5e380e21b9bb74e4aad14048c901a2ebcf29
podmar <marta.podziewska@gmail.com>
2022-09-16 09:29:18 +0200

    extracted functions and added an endless loop to the main function

--- main.go
+++ main.go
@@ -2,73 +2,111 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"time"
 )
 
 const barkSound = "woof"
+const dogName = "Lea"
+const maxBellyRubs uint = 5
+const numberOfBarks uint = 50
+
+var remainingBarks uint = 49
 
 func main() {
-	// var dogName = "Lea" // is equal to -->
-	dogName := "Lea" // create a variable and assign a value, no constants and no possibility to explicitly declare a type
-	// used for short lived variables, result of a function call or an iterator in a function
 
-	const numberOfBarks = 50     // total number of barks, type defined implicitly
-	var remainingBarks uint = 50 // explicit specification mostly used when type is to be different than the default, uint is a non-negative number
+	greetUser()
+	userName := gatherUserName()
 
+	for {
+		userBellyRubs := askForRubs()
+		rubs := verifyRubs(userBellyRubs)
+		rubBelly(rubs)
+		bark(rubs)
+
+		remainingBarks -= rubs
+
+		if remainingBarks == 0 {
+			fmt.Printf("Sorry %s, I cannot bark any more, I sleep now. Bye.", userName)
+			break
+		}
+
+		displayRemainingBarks()
+	}
+
+}
+
+func greetUser() {
 	fmt.Println("Hello World, I am", dogName)
-	fmt.Printf("I have %v loud barks at my disposal, %s, and %v are left. \n", numberOfBarks, barkSound, remainingBarks)
+	fmt.Printf("I have %v loud barks at my disposal, %s! ... and %v are left. \n", numberOfBarks, barkSound, remainingBarks)
+}
 
+func gatherUserName() string {
 	var userName string
-	// is the same as var userName string = ""
-	var bellyRubs uint
-	var maxBellyRubs uint = 5
-	// fmt.Printf("dogName is a %T, remainingBarks is type %T and userName is %T. \n", dogName, remainingBarks, userName)
 
-	// Scan take user input as a parameter
-	// a pointer directs to a memory address where a variable is stored, concept from C, C++ (not in java f.ex.)
-	// fmt.Println(&userName)
-
-	// direct Scan to the memory address
-	fmt.Println("Enter your first name: ")
+	fmt.Println("What's your first name?")
 	fmt.Scan(&userName)
 
 	fmt.Printf("Hi %v! Nice to meet you. \n", userName)
+
+	return userName
+}
+
+func askForRubs() uint {
+	var bellyRubs uint
+
 	fmt.Printf("How many belly rubs would you like to offer? You may offer between 0 and %v rubs at a time. \n", maxBellyRubs)
 	fmt.Scan(&bellyRubs)
 	fmt.Printf("You've offered %v belly rubs.\n", bellyRubs)
 
-	if bellyRubs > 5 {
+	return bellyRubs
+}
+
+func verifyRubs(bellyRubs uint) uint {
+	if bellyRubs > maxBellyRubs {
 		fmt.Printf("This is way more than your neighbours can take! You can only rub the belly %v times at once. \n", maxBellyRubs)
 		bellyRubs = maxBellyRubs
-		remainingBarks -= maxBellyRubs
 	}
 
 	if bellyRubs > remainingBarks {
-		fmt.Println("I can't take that much belly rubbing now, I need to sleep first.")
+		fmt.Printf("I can't take that much belly rubbing now, you may rub %v times. \n", remainingBarks)
 		bellyRubs = remainingBarks
-		remainingBarks = 0
 	}
 
+	return bellyRubs
+}
+
+func rubBelly(bellyRubs uint) {
+
 	fmt.Println("Rubbing the belly...")
-	var loadingRubbingSigns = []string{"/", "-", "\\", "|"}
-	for i := 0; i <= int(bellyRubs); i++ {
-		for _, sign := range loadingRubbingSigns {
+
+	showLoader(bellyRubs)
+
+	fmt.Println("\rRubbing complete. Initiating barking.")
+	time.Sleep(500 * time.Millisecond)
+}
+
+func showLoader(times uint) {
+	var loadingSigns = []string{"/", "-", "\\", "|"}
+	for i := 0; i <= int(times); i++ {
+		for _, sign := range loadingSigns {
 			fmt.Printf("\r %v", sign)
 			time.Sleep(150 * time.Millisecond)
 		}
 	}
+}
 
-	fmt.Println("\rRubbing complete. Initiating barking. ")
-	time.Sleep(500 * time.Millisecond)
+func bark(times uint) {
+	fmt.Print("\n")
 
-	var barks []string
-
-	for i := 0; i < int(bellyRubs); i++ {
-		barks = append(barks, barkSound)
+	for i := 0; i < int(times); i++ {
+		fmt.Printf("%s! ", barkSound)
+		time.Sleep(150 * time.Millisecond)
 	}
 
-	barkingString := strings.Join(barks, ", ")
-	fmt.Printf("\n%s! \n\n", barkingString)
+	fmt.Print("\n")
+	time.Sleep(150 * time.Millisecond)
+}
 
+func displayRemainingBarks() {
+	fmt.Printf("\nThat was some premium rubbing! I have %v barks left. \n\n", remainingBarks)
 }
